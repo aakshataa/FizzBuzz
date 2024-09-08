ae036bc6094efa9f285f89033d623a6abc89c31d
aakshataa <121204544+aakshataa@users.noreply.github.com>
2024-09-08 17:14:05 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -30,5 +30,38 @@ class FizzBuzz {
 
             }
         }
+        
+        // Second version of FizzBuzz, iterating only 20 times
+        
+        int i = 1;
+        
+        while (i < 100){
+            doFizzBuzz(i);
+
+            i++;
+        }
+    }
+
+    private static void doFizzBuzz(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        if (divisibleBy3 && divisibleBy5){
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else {
+
+            System.out.println(i);
+
+        }
     }
 }
